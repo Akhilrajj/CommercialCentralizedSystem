5c2bf35efe8a509ae04ea1231f2454861bc25d25
akhilnarayanoju <akhilnarayanoju@commercecx.com>
2025-04-28 17:14:30 +0530

    removing standalone as we are grouping this component into module

--- src/app/employee-management-system/add-edit-employee/add-edit-employee.component.ts
+++ src/app/employee-management-system/add-edit-employee/add-edit-employee.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-edit-employee',
-  standalone: false,
   templateUrl: './add-edit-employee.component.html',
   styleUrl: './add-edit-employee.component.scss'
 })
