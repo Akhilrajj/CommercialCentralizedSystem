e1264280217c2e4581bf7f942fcb6bdb87eafa55
akhilnarayanoju <akhilnarayanoju@commercecx.com>
2025-04-28 16:55:27 +0530

    adding emps module and add-edit-component

---
+++ src/app/employee-management-system/employee-management-system.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddEditEmployeeComponent } from './add-edit-employee/add-edit-employee.component';
+
+
+
+@NgModule({
+  declarations: [AddEditEmployeeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmployeeManagementSystemModule { }
